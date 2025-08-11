907f82f672f261ca11cc9c9cebb1177ec086543f
sontarou2028 <minsou2128@gmail.com>
2025-08-11 21:24:04 +0900

    add SenderHost.cs

---
+++ src/main/SenderHost.cs
@@ -0,0 +1,97 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace AutoAppdater.MainSenderHost
+{
+    internal struct SystemId
+    {
+        public const int System = 0;
+        public const int Log = 1;
+        public const int Console = 2;
+    }
+    public struct Author
+    {
+        
+    }
+    public class CopyData
+    {
+        public Author LibraryAuthor{ get; }
+        public int SenderId { get; }
+        public int? Int { get; }
+        public long? Long { get; }
+        public float? Float { get; }
+        public double? Double { get; }
+        public char? Char { get; }
+        public string? Str { get; }
+        public CopyData(int SenderId, int? Int, long? Long, float? Float, double? Double, char? Char, string? Str)
+        {
+            this.SenderId = SenderId;
+            this.Int = Int;
+            this.Long = Long;
+            this.Float = Float;
+            this.Double = Double;
+            this.Char = Char;
+            this.Str = Str;
+        }
+    }
+    public class CourierData : CopyData
+    {
+        public CourierData(int SenderId, int? Int, long? Long, float? Float, double? Double, char? Char, string? Str)
+        : base(SenderId, Int, Long, Float, Double, Char, Str)
+        {
+            //senderId check
+            if (SenderId == SystemId.System) throw new InvalidOperationException("Cannot use a system reserved ID.");
+            if (SenderId == SystemId.Log) throw new InvalidOperationException("Cannot use a system reserved ID.");
+            if (SenderId == SystemId.Console) throw new InvalidOperationException("Cannot use a system reserved ID.");
+        }
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct COPYDATASTRUCT32
+    {
+        public UInt32 dwData;
+        public UInt32 cbData;
+        public IntPtr lpData;
+    }
+    internal static class MainSender
+    {
+        const uint WM_COPYDATA = 0x004A;
+        const string MainProcess_Name = "AutoAppdater";
+        const string MainProcess_MainWindowName = "MainWindow";
+        //const int Send_Success = 0;
+        const int Send_Error_MainProc_NotFound = -1;
+        const int Send_Error_MainProc_IllegalTypeOrVersion = -2;
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern Int32 SendMessage(IntPtr hWnd, uint Msg, Int32 wParam, ref COPYDATASTRUCT32 lParam);
+        internal static int Send(CopyData data)
+        {
+            //check
+            Process[] p = Process.GetProcessesByName(MainProcess_Name);
+            if (p.Length == 0) return Send_Error_MainProc_NotFound;
+            if (p[0].MainWindowTitle != MainProcess_MainWindowName) return Send_Error_MainProc_IllegalTypeOrVersion;
+            //obj
+            string cobj = JsonSerializer.Serialize(data);
+            COPYDATASTRUCT32 cd = new COPYDATASTRUCT32();
+            cd.dwData = 0;
+            cd.cbData = (UInt32)cobj.Length - 1;
+            cd.lpData = Marshal.StringToHGlobalAnsi(cobj);
+            SendMessage(p[0].MainWindowHandle, WM_COPYDATA, 0,ref cd);
+            return Marshal.GetLastWin32Error();
+        }
+        public static int Send(CourierData data)
+        {
+            //check
+            Process[] p = Process.GetProcessesByName(MainProcess_Name);
+            if (p.Length == 0) return Send_Error_MainProc_NotFound;
+            if (p[0].MainWindowTitle != MainProcess_MainWindowName) return Send_Error_MainProc_IllegalTypeOrVersion;
+            //obj
+            string cobj = JsonSerializer.Serialize(data);
+            COPYDATASTRUCT32 cd = new COPYDATASTRUCT32();
+            cd.dwData = 0;
+            cd.cbData = (UInt32)cobj.Length - 1;
+            cd.lpData = Marshal.StringToHGlobalAnsi(cobj);
+            SendMessage(p[0].MainWindowHandle, WM_COPYDATA, 0,ref cd);
+            return Marshal.GetLastWin32Error();
+        }
+    }
+}
